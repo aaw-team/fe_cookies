87d9c3c317790f2e33699cab186329a0aa813f75
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2018-06-15 19:07:41 +0200

    [TASK] Compatibility: decrease required php version
    => compatibility branch only

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.4.0',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.6.0-7.2.999',
+            'php' => '5.4.0-7.2.999',
             'typo3' => '6.2.31-9.2.999',
         ),
         'conflicts' => array(),
