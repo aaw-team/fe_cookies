0d819ff21690c6b5d9945b65ad6919b01d4fc471
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2020-04-23 02:15:49 +0200

    [TASK] Call registerPlugin without vendor name for TYPO3 >= 10.1
    See https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.1/Deprecation-88995-CallingRegisterPluginWithVendorName.html

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,8 +7,13 @@
  */
 
 // Register the frontend plugin
+$extensionName = 'FeCookies';
+// Prepend the vendor name for TYPO3 versions below 10.1
+if (version_compare(TYPO3_version, '10.1', '<')) {
+    $extensionName = 'AawTeam.' . $extensionName;
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'AawTeam.FeCookies',
+    $extensionName,
     'fecookies',
     'LLL:EXT:fe_cookies/Resources/Private/Language/locallang_db.xlf:plugin.fecookies.title',
     'EXT:fe_cookies/Resources/Public/Icons/Extension.svg'
